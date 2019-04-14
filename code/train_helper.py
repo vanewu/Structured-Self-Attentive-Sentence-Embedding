a7abc676b61363c6dc3e1167c8d7e412454e29bc
kenjewu <kenjewu@outlook.com>
2019-04-14 19:13:12 +0800

    rewrite code, export best model

--- code/train_helper.py
+++ code/train_helper.py
@@ -15,9 +15,13 @@
 # specific language governing permissions and limitations
 # under the License.
 
-# Function used for auxiliary training
 # author:kenjewu
 
+"""Function used for auxiliary training
+"""
+
+
+import os
 from time import time
 
 import mxnet as mx
@@ -26,133 +30,163 @@ from mxnet import autograd, gluon, nd
 from sklearn.metrics import accuracy_score, f1_score
 
 
-def train(
-        data_iter_train, data_iter_valid, model, loss, trainer, CTX, num_epochs, penal_coeff=0.0, clip=None,
-        class_weight=None, loss_name='wsce'):
-    '''
-    Function used in training
-    Args:
-        data_iter_train: the iter of training data
-        data_iter_valid: the iter of validation data
-        model: model to train
-        loss: loss function
-        trainer: the way of train
-        CTX: context
-        num_epochs: number of total epochs
-        penal_coeff: Penalty factor, default is 0.0
-        clip: gradient clipping threshold, default is None
-        class_weight: the weight of every class, default is None
-        loss_name: the name of loss function, default is 'wsce'
-    '''
-    print('Train on ', CTX)
+def calculate_loss(x, y, model, loss, loss_name, class_weight, penalization_coeff):
+    """calculate loss value
 
-    for epoch in range(1, num_epochs + 1):
+    Args:
+        x (NDArray): intput of model
+        y (NDArray): target
+        model (Block): model
+        loss (gluon.loss): loss function
+        loss_name (str): name of loss function
+        class_weight (NDArray): weight of sample loss value for each category
+        penalization_coeff (float): Attention penalty coefficient
+
+    Returns:
+        NDArray: output of model
+        NDArray: loss value
+    """
+
+    pred, att = model(x)
+    if loss_name == 'sce':
+        l = loss(pred, y)
+    elif loss_name == 'wsce':
+        l = loss(pred, y, class_weight, class_weight.shape[0])
+
+    # penalty
+    diversity_penalty = nd.batch_dot(att, nd.transpose(att, axes=(0, 2, 1))
+                                     ) - nd.eye(att.shape[1], ctx=att.context)
+    l = l + penalization_coeff * diversity_penalty.norm(axis=(1, 2))
+
+    return pred, l
+
+
+def train(train_dataloader, valid_dataloader, model, loss, trainer, ctx,
+          nepochs, penalization_coeff, clip, class_weight, loss_name,
+          model_root, model_name):
+    """Function used in training
+
+    Args:
+        train_dataloader (Dataloader): data loader of train data set
+        valid_dataloader (Dataloader): data loader of valid data set
+        model (Block): model
+        loss (gluon.loss): loss function
+        trainer (gluon.Trainer): trainer
+        ctx (context): mx.gpu() or mx.cpu()
+        nepochs (int): number of epoch
+        penalization_coeff (float): attention penalty coefficient
+        clip (float): value of gradient clip
+        class_weight (NDArray): weight of sample loss value for each category
+        loss_name (str): name of loss function
+        model_root (str): root path of model
+        model_name (str): name of model
+    """
+
+    print('Train on ', ctx)
+
+    parameters = [p for p in model.collect_params().values() if p.grad_req != 'null']
+    for epoch in range(1, nepochs + 1):
         start = time()
+        best_F1_valid = 0.
         train_loss = 0.
         total_pred = []
         total_true = []
-        n_batch = 0
 
-        for batch_x, batch_y in data_iter_train:
+        for nbatch, (batch_x, batch_y) in enumerate(train_dataloader):
+            batch_x, batch_y = batch_x.as_in_context(ctx), batch_y.as_in_context(ctx)
             with autograd.record():
-                batch_pred, att_output = model(batch_x)
-                if loss_name == 'sce':
-                    l = loss(batch_pred, batch_y)
-                elif loss_name == 'wsce':
-                    l = loss(batch_pred, batch_y, class_weight,
-                             class_weight.shape[0])
-
-                # penalty (惩罚项)
-                temp = nd.batch_dot(att_output, nd.transpose(att_output, axes=(0, 2, 1))
-                                    ) - nd.eye(att_output.shape[1], ctx=CTX)
-                l = l + penal_coeff * temp.norm(axis=(1, 2))
+                batch_pred, l = calculate_loss(batch_x, batch_y, model, loss,
+                                               loss_name, class_weight, penalization_coeff)
             l.backward()
 
-            # clip gradient (梯度裁剪)
-            clip_params = [p.data() for p in model.collect_params().values()]
+            # clip gradient
             if clip is not None:
-                norm = nd.array([0.0], CTX)
-                for param in clip_params:
-                    norm += (param.grad ** 2).sum()
-                norm = norm.sqrt().asscalar()
-                if norm > clip:
-                    for param in clip_params:
-                        param.grad[:] *= clip / norm
+                grads = [p.grad(ctx) for p in parameters]
+                gluon.utils.clip_global_norm(grads, clip)
 
             # update trainable params (更新参数)
             trainer.step(batch_x.shape[0])
 
-            batch_pred = np.argmax(nd.softmax(
-                batch_pred, axis=1).asnumpy(), axis=1)
+            batch_pred = np.argmax(nd.softmax(batch_pred, axis=1).asnumpy(), axis=1)
             batch_true = np.reshape(batch_y.asnumpy(), (-1, ))
             total_pred.extend(batch_pred.tolist())
             total_true.extend(batch_true.tolist())
             batch_train_loss = l.mean().asscalar()
 
-            n_batch += 1
             train_loss += batch_train_loss
 
-            if n_batch % 400 == 0:
+            if (nbatch + 1) % 400 == 0:
                 print('epoch %d, batch %d, bach_train_loss %.4f, batch_train_acc %.3f' %
-                      (epoch, n_batch, batch_train_loss, accuracy_score(batch_true, batch_pred)))
+                      (epoch, nbatch + 1, batch_train_loss, accuracy_score(batch_true, batch_pred)))
 
-        F1_train = f1_score(np.array(total_true), np.array(
-            total_pred), average='weighted')
+        # metric on total train data set
+        F1_train = f1_score(np.array(total_true), np.array(total_pred), average='weighted')
         acc_train = accuracy_score(np.array(total_true), np.array(total_pred))
-        train_loss /= n_batch
+        train_loss /= (nbatch + 1)
 
-        F1_valid, acc_valid, valid_loss = evaluate(
-            data_iter_valid, model, loss, penal_coeff, class_weight, loss_name)
+        # metrics on valid data set
+        F1_valid, acc_valid, valid_loss = evaluate(valid_dataloader, model, loss, loss_name,
+                                                   penalization_coeff, class_weight, ctx)
 
-        print('epoch %d, learning_rate %.5f \n\t train_loss %.4f, acc_train %.3f, F1_train %.3f, ' %
+        print('epoch % d, learning_rate % .5f \n\t train_loss % .4f,'
+              ' acc_train %.3f, F1_train %.3f, ' %
               (epoch, trainer.learning_rate, train_loss, acc_train, F1_train))
         print('\t valid_loss %.4f, acc_valid %.3f, F1_valid %.3f, '
               '\ntime %.1f sec' % (valid_loss, acc_valid, F1_valid, time() - start))
-        print('='*50)
 
-        # learning rate decay (学习率衰减)
+        # learning rate decay
         if epoch % 2 == 0:
             trainer.set_learning_rate(trainer.learning_rate * 0.9)
 
+        # save best model structure and parameters
+        if F1_valid > best_F1_valid:
+            best_F1_valid = F1_valid
+            best_epoch = epoch
 
-def evaluate(data_iter_valid, model, loss, penal_coeff=0.0, class_weight=None, loss_name='wsce'):
-    '''
-    the evaluation function
+            model_dir = model_root + os.path.sep + model_name
+            if not os.path.exists(model_dir):
+                os.makedirs(model_dir)
+            model_path = os.path.join(model_dir, 'self_att_bilstm_model')
+            model.export(model_path)
+            print('best f1: %d, best epoch: %d, model structure'
+                  ' and parameters saved in: %s' % (best_F1_valid, best_epoch, model_path))
+        print('=' * 50)
+
+
+def evaluate(valid_dataloader, model, loss, loss_name, penalization_coeff, class_weight, ctx):
+    """the evaluation function
+
     Args:
-        data_iter_valid: the iter of validation data
-        model: model to train
-        loss: loss function
-        penal_coeff: Penalty factor, default is 0.0
-        class_weight: the weight of every class, default is None
-        loss_name: the name of loss function, default is 'wsce'
+        valid_dataloader (Dataloader): data loader of valid data set
+        model (Block): model
+        loss (gluon.loss): loss function
+        loss_name (str): name of loss function
+        penalization_coeff (float): attention penalty coefficient
+        class_weight (NDArray): weight of sample loss value for each category
+        ctx (context): mx.gpu() or mx.cpu()
+
     Returns:
-        F1_valid: the f1 score
-        acc_valid: the accuracy score
-        valid_loss: the value of loss
-    '''
+        float: f1 score of data
+        float: accuracy score of data
+        float: loss value of data
+    """
+
     valid_loss = 0.
     total_pred = []
     total_true = []
-    n_batch = 1
-    for batch_x, batch_y in data_iter_valid:
-        batch_pred, att_output = model(batch_x)
-        if loss_name == 'sce':
-            l = loss(batch_pred, batch_y)
-        elif loss_name == 'wsce':
-            l = loss(batch_pred, batch_y, class_weight, class_weight.shape[0])
-        # penalty
-        temp = nd.batch_dot(att_output, nd.transpose(att_output, axes=(0, 2, 1))
-                            ) - nd.eye(att_output.shape[1], ctx=att_output.context)
-        l = l + penal_coeff * temp.norm(axis=(1, 2))
+
+    for nbatch, (batch_x, batch_y) in enumerate(valid_dataloader):
+        batch_x, batch_y = batch_x.as_in_context(ctx), batch_y.as_in_context(ctx)
+        batch_pred, l = calculate_loss(batch_x, batch_y, model, loss,
+                                       loss_name, class_weight, penalization_coeff)
         total_pred.extend(np.argmax(nd.softmax(
             batch_pred, axis=1).asnumpy(), axis=1).tolist())
         total_true.extend(np.reshape(batch_y.asnumpy(), (-1,)).tolist())
-        n_batch += 1
+        nbatch += 1
         valid_loss += l.mean().asscalar()
 
-    F1_valid = f1_score(np.array(total_true), np.array(
-        total_pred), average='weighted')
+    F1_valid = f1_score(np.array(total_true), np.array(total_pred), average='weighted')
     acc_valid = accuracy_score(np.array(total_true), np.array(total_pred))
-    valid_loss /= n_batch
+    valid_loss /= nbatch
 
     return F1_valid, acc_valid, valid_loss
